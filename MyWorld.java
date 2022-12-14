abfb5b06598cceee2543ee0243963d10c01857b9
341037307 <341037307@gapps.yrdsb.ca>
2022-12-14 14:36:17 -0500

    yay

--- MyWorld.java
+++ MyWorld.java
@@ -12,6 +12,7 @@ public class MyWorld extends World
     
     SimpleTimer timeClass = new SimpleTimer();
     boolean rythmMode = false;
+    boolean gameEnded = false;
     
     public static int score = 0;
     public Label scoreLabel;
@@ -32,11 +33,11 @@ public class MyWorld extends World
         addObject(elephant, 300, 300);
         
         scoreLabel = new Label(0, 70);
-	addObject(scoreLabel, 20, 20);
+        addObject(scoreLabel, 20, 20);
         
         timeClass.mark();
         spawnApple();
-	score = 0;
+        score = 0;
     }
     public void act()
     {   
@@ -45,29 +46,38 @@ public class MyWorld extends World
             spawnApple();
             timeClass.mark();
         }
-
+        
+        if (gameEnded == true && Greenfoot.isKeyDown("space"))
+        {
+            gameEnded = false;
+            spawnApple();
+            //timeClass.mark();
+        }
     }
 
     public void increaseScore()
     {
         score += 1;
         scoreLabel.setValue(score);
-	
-	if(score % 5 == 0)
-	{
-	    level += 1;
-	}
+    
+        if(score % 5 == 0)
+        {
+            level += 1;
+        }
     }
     public void gameOver()
     {
-	Label gameOverLabel = new Label("Game Over", 100);
-	addObject(gameOverLabel, getWidth()/2, getHeight()/2);
+        Label gameOverLabel = new Label("Space to Restart", 100);
+        addObject(gameOverLabel, getWidth()/2, getHeight()/2);
+        Label subtext = new Label("Subtext", 80);
+        addObject(gameOverLabel, getWidth()/3, getHeight()/3);
+        gameEnded = true;
     }    
 
     public void spawnApple()
     {
         Apple apple = new Apple();
-	apple.setSpeed(level);
+        apple.setSpeed(level);
         int x = (Greenfoot.getRandomNumber(4)+1)*100;
         int y = 0;
         addObject(apple, x, y);
--- apple.java
+++ apple.java
@@ -12,11 +12,11 @@ public class Apple extends Actor
      * Act - do whatever the apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    int speed = 1;
+    int speed = 7;
     public void act() 
     {
         // Add your action code here.
-        setLocation(getX(), getY() + 3);
+        setLocation(getX(), getY() + speed);
         int bottomY = getWorld().getHeight();
         if (getY() > bottomY)
         {
@@ -27,6 +27,6 @@ public class Apple extends Actor
     }    
     public void setSpeed(int spd) 
     {
-	speed = spd;
+	speed += spd;
     }
 }
